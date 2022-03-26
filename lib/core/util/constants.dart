95f59510591ad385b735ee5e85ad72f97102bb53
BabishShrestha <babishshrestha8@gmail.com>
2022-03-26 20:59:39 +0545

    Contains all the commonly used colors, Textstyles, padding, margin , Textfield decoration, container decoration, and more

---
+++ lib/core/util/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+//colors
+const kAppBarColor = Color(0xff24627F);
+const kUnselectedColor = Colors.grey;
+const kPrimaryScaffoldColor = Color(0xffECF7FB);
+const kBorderColor = Colors.blue;
+const kLocationColor = Colors.green;
+
+//margin and padding
+const kAppBarMargin = EdgeInsets.only(bottom: 60, left: 10, right: 10);
+
+const EdgeInsets kCardTileMargin = EdgeInsets.all(15);
+
+//textstyle
+const kCardTitleStyle = TextStyle(color: Colors.blue, fontSize: 14);
+
+//border radius
+const kClipRRectBorderRaduius = BorderRadius.vertical(top: Radius.circular(30));
+
+//container Decoration
+const kCurveHomePageDecoration = BoxDecoration(
+  color: kPrimaryScaffoldColor,
+  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+);
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  hintText: 'Enter Nearby Blood Bank',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(15.0),
+      ),
+      borderSide: BorderSide.none),
+);
