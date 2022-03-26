5f7cd21a1f966a2f635b64e6d0151ed37058bc1b
BabishShrestha <babishshrestha8@gmail.com>
2022-03-26 21:04:03 +0545

    Contains all the needed widget imports, needed for the nearby_blood_bank_page.dart

---
+++ lib/bloodbanknearby/presentation/widgets/widgets.dart
@@ -0,0 +1,8 @@
+export 'apptitle_widget.dart';
+export 'blood_bank_details.dart';
+export 'blood_bank_view_card.dart';
+export 'bottom_navbar.dart';
+export 'cached_image.dart';
+export 'not_found__widget.dart';
+export 'search_bar.dart';
+export 'title_sliverheaderdelegate.dart';
