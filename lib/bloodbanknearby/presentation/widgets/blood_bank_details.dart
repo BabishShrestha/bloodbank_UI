e1fd78cd702210544a3d1644317aa1d074467331
BabishShrestha <babishshrestha8@gmail.com>
2022-03-26 20:44:49 +0545

    Refactored the text details of nearby_blood_bank_page.dart into blood_bank_details.dart that has RichText and icon

---
+++ lib/bloodbanknearby/presentation/widgets/blood_bank_details.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/util/constants.dart';
+import '../../domain/entities/blood_bank.dart';
+
+class BloodBankDetails extends StatelessWidget {
+  const BloodBankDetails({
+    Key? key,
+    required this.bloodBank,
+  }) : super(key: key);
+
+  final BloodBank bloodBank;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        RichText(
+            textAlign: TextAlign.left,
+            text: TextSpan(
+                text: "${bloodBank.name}\n",
+                style: kCardTitleStyle,
+                children: [
+                  TextSpan(
+                    text: "Blood Group:  ${bloodBank.blood_group}\n",
+                    style: kCardTitleStyle.copyWith(
+                        fontSize: 12, color: kUnselectedColor),
+                  ),
+                  TextSpan(
+                    text: 'Phone Number: ${bloodBank.phone}',
+                    style: kCardTitleStyle.copyWith(
+                        fontSize: 12, color: kUnselectedColor),
+                  ),
+                ])),
+        Row(
+          children: [
+            const Icon(
+              Icons.location_on,
+              color: kLocationColor,
+            ),
+            Text(
+              '${bloodBank.location} Km far away',
+              style:
+                  kCardTitleStyle.copyWith(fontSize: 12, color: kLocationColor),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
